61c8d99090a9a8e0bb1ce17686335cf36fb32abd
salcrom <seya.28@gmail.com>
2022-01-19 10:12:21 +0100

    Fin del patron

--- src/02-component-patterns/components/ProductCard.tsx
+++ src/02-component-patterns/components/ProductCard.tsx
@@ -1,7 +1,7 @@
-import { createContext, CSSProperties, ReactElement } from 'react';
+import { createContext, CSSProperties } from 'react';
 
 import { useProduct } from '../hooks/useProduct';
-import { Product, ProductContextProps, onChangeArgs } from '../interfaces/interfaces';
+import { Product, ProductContextProps, onChangeArgs, InitialValues, ProductCardHandlers } from '../interfaces/interfaces';
 
 import styles from '../styles/styles.module.css';
 
@@ -9,31 +9,41 @@ import styles from '../styles/styles.module.css';
 export const ProductContext = createContext({} as ProductContextProps);
 const { Provider } = ProductContext;
 
-
 export interface Props {
     product: Product;
-    children?: ReactElement | ReactElement[];
+    // children?: ReactElement | ReactElement[];
+    children: ( args: ProductCardHandlers ) => JSX.Element,
     className?:string;
     style?: CSSProperties;
     onChange?: ( args: onChangeArgs ) => void;
     value?: number;
+    initialValues?: InitialValues;
 }
 
-export const ProductCard = ({ children, product, className, style, onChange, value }: Props ) => {
+export const ProductCard = ({ children, product, className, style, onChange, value, initialValues }: Props ) => {
 
-    const { counter , increaseBy } = useProduct({ onChange, product, value });
+    const { counter , increaseBy, maxCount, isMaxCountReached, reset } = useProduct({ onChange, product, value, initialValues });
 
     return (
         <Provider value={{
             counter,
             increaseBy,
+            maxCount,
             product,
         }}>
             <div
                 className={ `${ styles.productCard} ${ className }` }
                 style={ style }
             >
-                { children }
+                { children({
+                    count: counter,
+                    isMaxCountReached,
+                    maxCount: initialValues?.maxCount,
+                    product,
+
+                    increaseBy,
+                    reset
+                }) }
             </div>
         </Provider>
     )
--- src/02-component-patterns/interfaces/interfaces.ts
+++ src/02-component-patterns/interfaces/interfaces.ts
@@ -11,7 +11,9 @@ export interface Product {
 
 export interface ProductContextProps {
     counter: number;
+    maxCount?: number;
     product: Product;
+
     increaseBy: ( value: number ) => void;
 }
 
@@ -29,4 +31,19 @@ export interface onChangeArgs {
 
 export interface ProductInCart extends Product {
     count: number
+}
+
+export interface InitialValues {
+    count?: number;
+    maxCount?: number;
+}
+
+export interface ProductCardHandlers {
+    count: number;
+    isMaxCountReached: boolean;
+    maxCount?: number;
+    product: Product;
+
+    increaseBy: ( value: number ) => void;
+    reset: () => void;
 }
